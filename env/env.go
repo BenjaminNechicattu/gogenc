d2be3ce976f67102e7baaa96ed3dceae20b18791
Benjamin Nechicattu <benjaminnechicattu@gmail.com>
2025-03-31 10:52:23 +0530

    updation

--- env/env.go
+++ env/env.go
@@ -6,8 +6,23 @@ import (
 	"strconv"
 )
 
+// GetEnv is a helper function to retrieve the value of an environment variable.
 // GetEnv retrieves the value of an environment variable.
 // If the variable is not set, it returns the default value.
+// The type of the default value determines the expected type of the environment variable.
+// The function supports string, bool, int, and float64 types.
+// If the conversion fails, it returns the default value and an error.
+// Example usage: https://github.com/BenjaminNechicattu/gogenc/tree/main/examples
+//
+//	value, err := GetEnv("MY_ENV_VAR", "default")
+//	if err != nil {
+//
+// handle error
+//
+//	}
+//	fmt.Println(value) // prints the value of MY_ENV_VAR or "default" if not set
+//
+// python alternative is os.getenv("MY_ENV_VAR", "default"
 func GetEnv[T string | bool | int | float64](key string, defaultValue T) (T, error) {
 	value := os.Getenv(key)
 	if value == "" {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BenjaminNechicattu/gogenc
 
 go 1.24.1
+
+require github.com/BenjaminNechicattu/gogenc/compare v0.0.0-20250331045607-108c984fca58 // indirect
